0ea38c2563413b98a97183e40ac17089b84ee18e
Lexus10 <91476418+Lexus10@users.noreply.github.com>
2024-02-16 22:43:44 +0300

    Update index.cjs

--- src/index.cjs
+++ src/index.cjs
@@ -2,3 +2,12 @@
 	require('./bot.cjs'),
 	require('./server.cjs'),
 ].forEach((cmd) => cmd.start());
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Dark Uptime tarafından uptime edilmektedir.'));
+
+app.listen(port, () =>
+    console.log(`Bot bu adres üzerinde çalışıyor: http://localhost:${port}`)
+);
